6dba8fc0ceebc14fcf3fe2b921a70dc5df6349c2
noodles4liph <travis.haviv@gmail.com>
2025-11-26 16:12:16 -0500

    add hello.c

--- output/ftoc.c
+++ output/ftoc.c
@@ -5,32 +5,17 @@
 
 main()
 {
-  float fahr, celsius;
-  int lower, upper, step;
-  
-  lower = 0; // lower limit of temp scale
-  upper = 300; // upper limit of temp scale
-  step = 20; // step size
 
-  fahr = lower;
-  
-  
-printf("Fahrenheit to Celcious Table\n"); //header for f to c
+  int fahr;
 
-while (fahr <= upper) {
-    celsius = (5.0/9.0) * (fahr-32.0);
-    printf("%3.0f %6.2f\n", fahr, celsius);
-    fahr = fahr + step;
-  }  
+  printf("Ascending Temps\n");
 
-printf("Celsius to Fahrenheit Table\n"); //head for c to f
+  for (fahr = 0; fahr <= 300; fahr = fahr + 20)
+    printf("%3d %6.1f\n", fahr, (5.0/9.0) * (fahr-32));
 
-while(fahr >= lower) {
-    celsius = (5.0/9.0) * (fahr-32.0);
-    printf("%3.2f %6.0f\n", celsius, fahr);
-    fahr -= step;
-}
+  printf("Descending Temps\n");
 
-
+  for (fahr = 300; fahr >= 0; fahr = fahr - 20)
+    printf("%3d %6.1f\n", fahr, (5.0/9.0) * (fahr-32));
 }
 
